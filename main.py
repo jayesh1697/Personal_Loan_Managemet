884f0ee96035cb8a733e2717c8e7563e52b56d51
Jayesh Patel <jayeshpatel@192.168.0.106>
2023-10-29 22:25:04 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Login Form")
+root.iconbitmap("1486564172-finance-loan-money_81492.ico")
+# root.geometry("1350720+0+0")
+root.maxsize(1100,1100)
+
+
+root.mainloop()
